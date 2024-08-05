32df20cb810e519816b41e40cebd0d395299b4df
WKK <toutopia0@gmail.com>
2024-08-05 22:17:14 +0900

    수정의 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  build: {
+    extend(config, ctx) {},
+  },
+  runtimeConfig: {
+    public: {
+      PUPPETEER_EXECUTABLE_PATH: process.env.PUPPETEER_EXECUTABLE_PATH,
+    },
+  },
+});
